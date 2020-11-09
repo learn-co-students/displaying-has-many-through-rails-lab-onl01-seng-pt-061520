41c7d554848953e2556ae15d9d39ca70fc6b3b55
DeLaCruz26 <dametrik.fick@gmail.com>
2020-11-08 20:11:10 -0500

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,22 @@
+class PatientsController < ApplicationController
+    before_action :find_patient, only: [:show]
+
+    def index
+        @patients = Patient.all
+    end
+    
+
+    def show
+    end
+
+
+    private
+
+    def find_patient
+        @patient = Patient.find(params[:id])
+    end
+
+    def patient_params
+        params.required(:patient).permit(:name, :age)
+    end
+end
