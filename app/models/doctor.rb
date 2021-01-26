8a472a3ec24a5d5fbe7dea34e5b4e92e0a342957
Celina Jackson <65145705+CelinaJackson@users.noreply.github.com>
2021-01-26 15:10:28 -0500

    Update doctor.rb

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,4 @@
 class Doctor < ApplicationRecord
-    validates :name, presence: true
-    validates :department, presence: true
     has_many :appointments
     has_many :patients, through: :appointments
 end
