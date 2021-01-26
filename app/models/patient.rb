071c5bba788273cbb5c956c340e192813c2f3b81
Celina Jackson <65145705+CelinaJackson@users.noreply.github.com>
2021-01-26 15:09:51 -0500

    Update patient.rb

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,4 @@
 class Patient < ApplicationRecord
-    validates :name, presence: true 
-    validates :age, presence: true
-    has_many :doctors, through: :appointments
     has_many :appointments
+    has_many :doctors, through: :appointments
 end
